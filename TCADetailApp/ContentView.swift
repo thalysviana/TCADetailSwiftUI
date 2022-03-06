bf5787157f09245ae2c13f2c602d53cdb6c75a8f
Thalys Viana <thalysvianaguiar@gmail.com>
2022-03-06 16:52:49 -0300

    Initial Commit

---
+++ TCADetailApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCADetailApp
+//
+//  Created by Thalys Viana on 06/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCADetailApp/TCADetailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADetailAppApp.swift
+//  TCADetailApp
+//
+//  Created by Thalys Viana on 06/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TCADetailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
